a15c60c492b3ad960ca06f42defa6f789041eb19
Chbmleao <caikleao@gmail.com>
2024-05-23 22:20:16 -0300

    feat: get products from mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,28 @@
 import axios from "axios";
 import Table from "@/components/Table";
-
-const products = [
-  {
-    _id: "1",
-    description: "Product 1",
-    quantity: 10,
-    unity: "unit",
-    priceBought: 10,
-    supplier: "Supplier 1",
-  },
-  {
-    _id: "2",
-    description: "Product 2",
-    quantity: 20,
-    unity: "unit",
-    priceBought: 20,
-    supplier: "Supplier 2",
-  },
-  {
-    _id: "3",
-    description: "Product 3",
-    quantity: 30,
-    unity: "unit",
-    priceBought: 30,
-    supplier: "Supplier 3",
-  },
-];
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  async function createProduct(ev) {
-    ev.preventDefault();
-    const data = {
-      description: "Product 1",
-      quantity: 10,
-      unity: "unit",
-      priceBought: 10,
-      supplier: "Supplier 1",
-    };
-  
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []); // Isso garante que a função getProducts seja chamada apenas uma vez, quando o componente for montado
+
+  async function getProducts() {
     try {
-      await axios.post("/api/products", data);
+      const response = await axios.get("/api/products");
+      setProducts(response.data);
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <div className="w-full h-screen bg-blue-500">
-      <h1>Produtos</h1>
-      <button onClick={createProduct}>Criar produto</button>
+    <div className="w-full p-5 h-screen bg-blue-500">
+      <h1 className="font-bold text-white text-4xl p-7 mx-auto text-center">
+        Controle de Estoque
+      </h1>
       <div className="">
         <Table products={products} />
       </div>
@@ -57,11 +30,3 @@ export default function Home() {
   );
 }
 
-// export async function getServerSideProps() {
-//   const { data } = await axios.get("/api/products");
-//   return {
-//     props: {
-//       products: data,
-//     },
-//   };
-// }
