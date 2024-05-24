d3ab417a3c923149300114cd68640bdde7787015
Tarcizio Lafaiete <tarcizio.lafaiete@inventvision.com.br>
2024-05-24 16:45:32 -0300

    refactor(Extract Function): Extraindo funções para as requesições de acesso ao banco fornecidos pela api Products

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,61 +1,92 @@
 import { mongooseConnect } from "@/lib/mongoose"
 import Product from "@/models/Product"
+import { info } from "console"
+import { func } from "prop-types"
+
+async function postProductsDatabase(informations,res){
+  
+  const { description, quantity, unity, priceBought, supplier } = informations
+  try {
+    const productDoc = await Product.create({
+      description,
+      quantity,
+      unity,
+      priceBought,
+      supplier,
+    })
+    res.status(201).json(productDoc)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar o produto." })
+  }
+
+}
+
+async function getProductDatabse(res){
+
+  try {
+    const products = await Product.find({})
+    res.status(201).json(products)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar o produto." })
+  }
+
+}
+
+async function putProductDatabase(informations,res){
+
+  const { description, quantity, unity, priceBought, supplier } = informations
+    
+  try {
+    const updatedProduct = await Product.findOneAndUpdate(
+      { description: description },
+      { quantity, unity, priceBought, supplier },
+      { new: true }
+    )
+
+    if (!updatedProduct) {console.log(`No product found with description: ${description}`)}
+
+    console.log('Updated Product:', updatedProduct)
+    res.status(201).json(updatedProduct)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar o produto." })
+  }
+
+}
+
+async function deleteProductDatabase(description,res){
+
+  try {
+    const result = await Product.deleteOne({ 'test.description': description })
+    console.log(`This many objects deleted: ${result.deletedCount}`)
+    res.status(201).json(result.deletedCount)
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar o produto." })
+  }
+
+}
 
 export default async function RequestToProductsDatabase(req, res) {
   const { method } = req
   await mongooseConnect()
 
   if (method === "POST") {
-    const { description, quantity, unity, priceBought, supplier } = req.body
+    
+    postProductsDatabase(req.body,res);
 
-    try {
-      const productDoc = await Product.create({
-        description,
-        quantity,
-        unity,
-        priceBought,
-        supplier,
-      })
-      res.status(201).json(productDoc)
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o produto." })
-    }
   } else if (method === "DELETE") {
-    const { description } = req.body
+    
+    const { description } = req.body;
+    deleteProductDatabase(description,res);
 
-    try {
-      const result = await Product.deleteOne({ 'test.description': description })
-      console.log(`This many objects deleted: ${result.deletedCount}`)
-      res.status(201).json(result.deletedCount)
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o produto." })
-    }
   } else if (method === "GET") {
 
-    try {
-      const products = await Product.find({})
-      res.status(201).json(products)
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o produto." })
-    }
-  } else if (method === "PUT") {
-    const { description, quantity, unity, priceBought, supplier } = req.body
+    getProductDatabse(res);
     
-    try {
-      const updatedProduct = await Product.findOneAndUpdate(
-        { description: description },
-        { quantity, unity, priceBought, supplier },
-        { new: true }
-      )
-  
-      if (!updatedProduct) {console.log(`No product found with description: ${description}`)}
-  
-      console.log('Updated Product:', updatedProduct)
-      res.status(201).json(updatedProduct)
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o produto." })
-    }
+  } else if (method === "PUT") {
+
+    putProductDatabase(req.body,res);
+    
   } else {
-    res.status(405).json({ error: "Método não permitido." })
+    res.status(405).json({ error: "Método não permitido." });
   }
 }
