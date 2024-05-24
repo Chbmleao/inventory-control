92fefa624dde110d6faaa31ad3cd045900cf44a6
Chbmleao <caikleao@gmail.com>
2024-05-23 21:09:26 -0300

    feat: first table implmentation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,32 @@
 import axios from "axios";
+import Table from "@/components/Table";
+
+const products = [
+  {
+    _id: "1",
+    description: "Product 1",
+    quantity: 10,
+    unity: "unit",
+    priceBought: 10,
+    supplier: "Supplier 1",
+  },
+  {
+    _id: "2",
+    description: "Product 2",
+    quantity: 20,
+    unity: "unit",
+    priceBought: 20,
+    supplier: "Supplier 2",
+  },
+  {
+    _id: "3",
+    description: "Product 3",
+    quantity: 30,
+    unity: "unit",
+    priceBought: 30,
+    supplier: "Supplier 3",
+  },
+];
 
 export default function Home() {
   async function createProduct(ev) {
@@ -11,14 +39,29 @@ export default function Home() {
       supplier: "Supplier 1",
     };
   
-    await axios.post("/api/products", data);
+    try {
+      await axios.post("/api/products", data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
-    <>
-      <button onClick={createProduct}>
-        Click me!
-      </button>
-    </>
+    <div className="w-full h-screen bg-blue-500">
+      <h1>Produtos</h1>
+      <button onClick={createProduct}>Criar produto</button>
+      <div className="">
+        <Table products={products} />
+      </div>
+    </div>
   );
 }
+
+// export async function getServerSideProps() {
+//   const { data } = await axios.get("/api/products");
+//   return {
+//     props: {
+//       products: data,
+//     },
+//   };
+// }
