e5b53ae2fc43e2bb17f9d552fab316811b8570cb
GabrielBifano <gabrielfev@outlook.com>
2024-05-23 21:30:43 -0300

    This is the products

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,12 +1,12 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import Product from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose"
+import Product from "@/models/Product"
 
 export default async function handle(req, res) {
-  const { method } = req;
-  await mongooseConnect();
+  const { method } = req
+  await mongooseConnect()
 
   if (method === "POST") {
-    const { description, quantity, unity, priceBought, supplier } = req.body;
+    const { description, quantity, unity, priceBought, supplier } = req.body
 
     try {
       const productDoc = await Product.create({
@@ -15,12 +15,47 @@ export default async function handle(req, res) {
         unity,
         priceBought,
         supplier,
-      });
-      res.status(201).json(productDoc);
+      })
+      res.status(201).json(productDoc)
     } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o produto." });
+      res.status(500).json({ error: "Erro ao criar o produto." })
+    }
+  } else if (method === "DELETE") {
+    const { description } = req.body
+
+    try {
+      const result = await Product.deleteOne({ 'test.description': description })
+      console.log(`This many objects deleted: ${result.deletedCount}`)
+      res.status(201).json(result.deletedCount)
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao criar o produto." })
+    }
+  } else if (method === "GET") {
+
+    try {
+      const products = await Product.find({})
+      res.status(201).json(products)
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao criar o produto." })
+    }
+  } else if (method === "PUT") {
+    const { description, quantity, unity, priceBought, supplier } = req.body
+    
+    try {
+      const updatedProduct = await Product.findOneAndUpdate(
+        { description: description },
+        { quantity, unity, priceBought, supplier },
+        { new: true }
+      )
+  
+      if (!updatedProduct) {console.log(`No product found with description: ${description}`)}
+  
+      console.log('Updated Product:', updatedProduct)
+      res.status(201).json(updatedProduct)
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao criar o produto." })
     }
   } else {
-    res.status(405).json({ error: "Método não permitido." });
+    res.status(405).json({ error: "Método não permitido." })
   }
 }
